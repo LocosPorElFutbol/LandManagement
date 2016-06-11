01444f1c67c82a2f6773c28d0b7a2aab73fdf325
leo-9 <tec.lrodriguez@gmail.com>
2016-06-11 17:27:42 -0300

    Se refactorizo todo el codigo del formulario frmTipoFamiliarABM

--- LandManagement/frmTipoFamiliarABM.Designer.cs
+++ LandManagement/frmTipoFamiliarABM.Designer.cs
@@ -31,7 +31,7 @@
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.btnCancelar = new System.Windows.Forms.Button();
             this.btnGuardar = new System.Windows.Forms.Button();
-            this.txbDescripcionTFamiliar = new System.Windows.Forms.TextBox();
+            this.txbDescripcion = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
@@ -43,11 +43,11 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.groupBox1.Controls.Add(this.btnCancelar);
             this.groupBox1.Controls.Add(this.btnGuardar);
-            this.groupBox1.Controls.Add(this.txbDescripcionTFamiliar);
+            this.groupBox1.Controls.Add(this.txbDescripcion);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Location = new System.Drawing.Point(8, 45);
+            this.groupBox1.Location = new System.Drawing.Point(3, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(277, 115);
+            this.groupBox1.Size = new System.Drawing.Size(277, 89);
             this.groupBox1.TabIndex = 3;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Tipo de Familiar";
@@ -55,7 +55,7 @@
             // btnCancelar
             // 
             this.btnCancelar.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnCancelar.Location = new System.Drawing.Point(135, 77);
+            this.btnCancelar.Location = new System.Drawing.Point(183, 51);
             this.btnCancelar.Name = "btnCancelar";
             this.btnCancelar.Size = new System.Drawing.Size(75, 23);
             this.btnCancelar.TabIndex = 8;
@@ -65,36 +65,37 @@
             // 
             // btnGuardar
             // 
-            this.btnGuardar.Location = new System.Drawing.Point(54, 77);
+            this.btnGuardar.Location = new System.Drawing.Point(102, 51);
             this.btnGuardar.Name = "btnGuardar";
             this.btnGuardar.Size = new System.Drawing.Size(75, 23);
             this.btnGuardar.TabIndex = 7;
-            this.btnGuardar.Text = "Agregar";
+            this.btnGuardar.Text = "Guardar";
             this.btnGuardar.UseVisualStyleBackColor = true;
-            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click_1);
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
             // 
-            // txbDescripcionTFamiliar
+            // txbDescripcion
             // 
-            this.txbDescripcionTFamiliar.Location = new System.Drawing.Point(54, 25);
-            this.txbDescripcionTFamiliar.Multiline = true;
-            this.txbDescripcionTFamiliar.Name = "txbDescripcionTFamiliar";
-            this.txbDescripcionTFamiliar.Size = new System.Drawing.Size(179, 27);
-            this.txbDescripcionTFamiliar.TabIndex = 1;
+            this.txbDescripcion.Location = new System.Drawing.Point(81, 25);
+            this.txbDescripcion.Multiline = true;
+            this.txbDescripcion.Name = "txbDescripcion";
+            this.txbDescripcion.Size = new System.Drawing.Size(177, 20);
+            this.txbDescripcion.TabIndex = 1;
+            this.txbDescripcion.Validating += new System.ComponentModel.CancelEventHandler(this.ValidatingControl);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(12, 28);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.Size = new System.Drawing.Size(63, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Tipo:";
+            this.label1.Text = "Descripción";
             // 
             // frmTipoFamiliarABM
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(292, 270);
+            this.ClientSize = new System.Drawing.Size(292, 113);
             this.Controls.Add(this.groupBox1);
             this.Name = "frmTipoFamiliarABM";
             this.Text = "frmTipoFamiliarABM";
@@ -110,7 +111,7 @@
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Button btnCancelar;
         private System.Windows.Forms.Button btnGuardar;
-        private System.Windows.Forms.TextBox txbDescripcionTFamiliar;
+        private System.Windows.Forms.TextBox txbDescripcion;
         private System.Windows.Forms.Label label1;
     }
 }
--- LandManagement/frmTipoFamiliarABM.cs
+++ LandManagement/frmTipoFamiliarABM.cs
@@ -8,86 +8,127 @@ using System.Text;
 using System.Windows.Forms;
 using LandManagement.Business;
 using LandManagement.Entities;
+using log4net;
+using LandManagement.Utilidades;
 
 namespace LandManagement
 {
     public partial class frmTipoFamiliarABM : Form
     {
-        private TipoFamiliarBusiness tfamiliarBusiness;
-        private tbtipofamiliar tfamiliar;
-        private int idTFamiliar = 0;
+        public static readonly ILog log = log4net.LogManager.GetLogger
+            (System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        private tbtipofamiliar tipofamiliarExistente;
         private Form formPadre;
+        ValidarControles validarControles;
         public ErrorProvider errorProvider1 = new ErrorProvider();
+
         public frmTipoFamiliarABM(Form formularioPadre)
         {
             InitializeComponent();
             this.formPadre = formularioPadre;
         }
 
-        public frmTipoFamiliarABM(tbtipofamiliar tFamiliar, Form formularioPadre)
+        public frmTipoFamiliarABM(tbtipofamiliar _tipoFamiliar, Form _formularioPadre)
         {
-
             InitializeComponent();
 
-            tfamiliarBusiness = new TipoFamiliarBusiness();
-            this.idTFamiliar = tFamiliar.tif_id;
-            txbDescripcionTFamiliar.Text = tFamiliar.tif_descripcion;
-            this.formPadre = formularioPadre;
+            this.tipofamiliarExistente = _tipoFamiliar;
+            this.formPadre = _formularioPadre;
 
+            btnGuardar.Click -= new EventHandler(btnGuardar_Click);
+            btnGuardar.Click += new EventHandler(btnGuardarActualiza_Click);
         }
 
         private void frmTipoFamiliarABM_Load(object sender, EventArgs e)
         {
-            CargarBotones();
+            this.AutoValidate = System.Windows.Forms.AutoValidate.Disable;
+
+            if (this.getTipoFamiliar() != null)
+                CargarControles(this.getTipoFamiliar());
         }
 
         private void btnGuardar_Click(object sender, EventArgs e)
         {
             try
             {
-                CargaObjetoTFamiliar();
-                GuardaObjetoTFamiliar();
-                MensajeOk();
-                ((frmTipoFamiliar)formPadre).CargarGrilla();
-                this.Close();
+                if (this.ValidateChildren())
+                {
+                    tbtipofamiliar tipoFamiliar = new tbtipofamiliar();
+
+                    CargarObjeto(tipoFamiliar);
+                    GuardarObjeto(tipoFamiliar);
+                    MensajeOk();
+                    ((frmTipoFamiliar)formPadre).CargarGrilla();
+                    this.Close();
+                }
             }
             catch (Exception ex)
             {
-                throw ex;
+                log.Error(ex.Message);
+                if (ex.InnerException != null)
+                    log.Error(ex.InnerException.Message);
+                MessageBox.Show("Error al crear.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
-        private void CargarBotones()
+
+        private void btnGuardarActualiza_Click(object sender, EventArgs e)
         {
-            this.tfamiliar = new tbtipofamiliar();
-            if (this.idTFamiliar != 0)
+            try
             {
-                btnGuardar.Text = "Actualizar";
+                if (this.ValidateChildren())
+                {
+                    Cursor.Current = Cursors.WaitCursor;
+                    tbtipofamiliar tipoFamiliarLocal = new tbtipofamiliar();
+                    tipoFamiliarLocal = this.getTipoFamiliar();
+
+                    CargarObjeto(tipoFamiliarLocal);
+                    GuardarObjeto(tipoFamiliarLocal);
+                    MensajeOk();
+                    ((frmTipoFamiliar)formPadre).CargarGrilla();
+                    this.Close();
+
+                    Cursor.Current = Cursors.Default;
+                }
+            }
+            catch (Exception ex)
+            {
+                log.Error(ex.Message);
+                if (ex.InnerException != null)
+                    log.Error(ex.InnerException.Message);
+                MessageBox.Show("Error al actualizar.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
-        private void CargaObjetoTFamiliar()
+
+        private void btnCancelar_Click(object sender, EventArgs e)
         {
-            this.tfamiliar = new tbtipofamiliar();
-
-            if (this.idTFamiliar != 0)
-                this.tfamiliar.tif_id = this.idTFamiliar;
-
-            tfamiliar.tif_descripcion = txbDescripcionTFamiliar.Text;
+            MensajeCancelar();
         }
 
-        private void GuardaObjetoTFamiliar()
+        private void CargarControles(tbtipofamiliar _tipoFamiliar)
         {
-            tfamiliarBusiness = new TipoFamiliarBusiness();
+            txbDescripcion.Text = _tipoFamiliar.tif_descripcion;
+        }
 
-            if (this.idTFamiliar != 0)
-                tfamiliarBusiness.Update(this.tfamiliar);
+        private void CargarObjeto(tbtipofamiliar _tipoFamiliar)
+        {
+            _tipoFamiliar.tif_descripcion = txbDescripcion.Text;
+        }
+
+        private void GuardarObjeto(tbtipofamiliar _tipoFamiliar)
+        {
+            TipoFamiliarBusiness tipoFamiliarBusiness = new TipoFamiliarBusiness();
+            if (getTipoFamiliar() != null)
+                tipoFamiliarBusiness.Update(_tipoFamiliar);
             else
-                tfamiliarBusiness.Create(this.tfamiliar);
-        }
-        private void MensajeOk()
-        {
-            MessageBox.Show("El registro se guardo correctamente", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                tipoFamiliarBusiness.Create(_tipoFamiliar);
         }
 
+        public tbtipofamiliar getTipoFamiliar()
+        {
+            return this.tipofamiliarExistente;
+        }
+
+        #region Mensajes de Pantalla
         private void MensajeCancelar()
         {
             DialogResult dialogResult = DialogResult.None;
@@ -97,30 +138,38 @@ namespace LandManagement
 
             if (dialogResult == System.Windows.Forms.DialogResult.Yes)
             {
+                // Stop the validation of any controls so the form can close.
                 AutoValidate = AutoValidate.Disable;
                 this.Close();
             }
         }
 
-        private void btnGuardar_Click_1(object sender, EventArgs e)
+        private void MensajeOk()
         {
-            try
-            {
-                CargaObjetoTFamiliar();
-                GuardaObjetoTFamiliar();
-                MensajeOk();
-                ((frmTipoFamiliar)formPadre).CargarGrilla();
-                this.Close();
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
+            MessageBox.Show("El registro se guardo correctamente", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
+        #endregion
 
-        private void btnCancelar_Click(object sender, EventArgs e)
+        #region Validación de controles
+        private void ValidatingControl(object sender, CancelEventArgs e)
         {
-            MensajeCancelar();
+            errorProvider1.BlinkStyle = ErrorBlinkStyle.NeverBlink;
+            validarControles = new ValidarControles();
+            Control control = validarControles.ObtenerControl(sender);
+            string error = validarControles.ValidarControl(sender);
+
+            if (!string.IsNullOrEmpty(error))
+            {
+                errorProvider1.SetError(control, error);
+
+                //Me valida hasta ingresar el valor correcto
+                e.Cancel = true;
+                return;
+            }
+
+            errorProvider1.SetError(control, error);
         }
+        #endregion
+
     }
 }
